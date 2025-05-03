cfe5206cbae34262f512147995c787941fa2d522
Mostafa-Elnagar <mostafaelnagar.ds@gmail.com>
2025-05-03 16:50:54 +0300

    Initial commit

---
+++ data_load.py
@@ -0,0 +1,128 @@
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine, types, text, inspect
+from tqdm import tqdm
+import config
+import re
+from warnings import filterwarnings
+filterwarnings('ignore')
+
+# -------------------- CONFIG --------------------
+CSV_PATH = "Crimes_-_2001_to_Present.csv"
+
+DB_CONNECTION_STRING = (
+    f"mssql+pyodbc://{config.DB_USER}:{config.DB_PASSWORD}@{config.DB_SERVER}/"
+    f"{config.DB_NAME}?driver={config.DB_DRIVER.replace(' ', '+')}"
+)
+
+engine = create_engine(DB_CONNECTION_STRING, fast_executemany=True)
+
+CHUNKSIZE = 100_000
+
+
+# Function to enable IDENTITY_INSERT
+def enable_identity_insert(engine, table_name):
+    with engine.connect() as conn:
+        conn.execute(text(f"SET IDENTITY_INSERT {table_name} ON"))
+
+# Function to disable IDENTITY_INSERT
+def disable_identity_insert(engine, table_name):
+    with engine.connect() as conn:
+        conn.execute(text(f"SET IDENTITY_INSERT {table_name} OFF"))
+
+# -------------------- BUILD DIMENSION --------------------
+def build_dim(df, cols, table_name, engine):
+    dim_name = "_".join(table_name.split('_')[1:])
+    col_names = [col["name"] for col in inspect(engine).get_columns(table_name) if f'{dim_name}_id' != col["name"]]
+    dim = df[cols].drop_duplicates().reset_index(drop=True)
+    
+    dim = dim[cols]
+    dim.columns = col_names
+
+    dim.to_sql(table_name, engine, index=False, if_exists='append')
+
+    return dim
+
+# -------------------- BUILD DATE DIM --------------------
+def build_date_dim(df):
+    df['date_only'] = pd.to_datetime(df['Date']).dt.date
+    dim = pd.DataFrame(df['date_only'].unique(), columns=['full_date'])
+    # dim['date_id'] = dim['full_date'].apply(lambda d: int(d.strftime('%Y%m%d')))
+    dim['year'] = pd.to_datetime(dim['full_date']).dt.year
+    dim['month'] = pd.to_datetime(dim['full_date']).dt.month
+    dim['day'] = pd.to_datetime(dim['full_date']).dt.day
+    dim['day_of_week'] = pd.to_datetime(dim['full_date']).dt.day_name()
+    dim['is_weekend'] = dim['day_of_week'].isin(['Saturday', 'Sunday']).astype(int)
+
+    dim.to_sql("dim_date", engine, index=False, if_exists="append")
+
+    return dim
+
+# # -------------------- FIRST CHUNK (Build Dimensions) --------------------
+# reader = pd.read_csv(CSV_PATH, chunksize=CHUNKSIZE, parse_dates=["Date", "Updated On"], low_memory=False)
+# first_chunk = next(reader)
+
+# print("Loading dimensions...")
+# dim_date = build_date_dim(first_chunk)
+# dim_crime_type = build_dim(first_chunk, ['IUCR', 'Primary Type', 'Description', 'FBI Code'], 'dim_crime_type', engine)
+# dim_location = build_dim(first_chunk, ['Block', 'Ward', 'Community Area', 'Location Description', 'Latitude', 'Longitude'], 'dim_location', engine)
+# print("Dimensions Loaded.")
+
+# -------------------- RESET READER --------------------
+def normalize_column_names(df):
+    df.columns = [
+        re.sub(r'\W+', '_', col.strip().lower())  # Replace non-alphanumeric with underscore
+        for col in df.columns
+    ]
+    return df
+
+reader = pd.read_csv(CSV_PATH, chunksize=CHUNKSIZE, parse_dates=["Date"], low_memory=False)
+
+# -------------------- FACT LOAD --------------------
+print("Loading fact table into SQL Server...")
+for chunk in tqdm(reader):
+    chunk_length = len(chunk)
+    print(f"chunk size: {len(chunk)}")
+    print("Loading dimensions...")
+    dim_date = build_date_dim(chunk)
+    query = "SELECT * FROM dim_date"
+    dim_date = pd.read_sql(query, engine)
+    
+    dim_crime_type = build_dim(chunk, ['IUCR', 'Primary Type', 'Description', 'FBI Code'], 'dim_crime_type', engine)
+    query = "SELECT * FROM dim_crime_type"
+    dim_crime_type = pd.read_sql(query, engine)
+
+    dim_location = build_dim(chunk, ['Block', 'Ward', 'Community Area', 'Location Description', 'Latitude', 'Longitude'], 'dim_location', engine)
+    query = "SELECT * FROM dim_location"
+    dim_location = pd.read_sql(query, engine)
+
+    print("Dimensions Loaded.")
+
+    cols = [
+        'id', 'case_number', 'date_id', 'location_id',
+        'crime_type_id', "arrest", "domestic", 'beat', 'district'
+    ]
+    chunk = normalize_column_names(chunk)
+    chunk['date_only'] = chunk['date'].dt.date
+    print("Merging with dim_date...")
+    chunk = chunk.merge(dim_date, left_on='date_only', right_on='full_date', how='left')
+    print("Merging with dim_location...")
+    chunk = chunk.merge(dim_location, on=['longitude', 'latitude', "location_description", 'block', 'ward', 'community_area'], how='left')
+    print("Merging with dim_crime_type...")
+    chunk = chunk.merge(dim_crime_type, on=['iucr', 'primary_type', 'description', 'fbi_code'], how='left')
+    print("Merging completed.")
+
+    fact = chunk[cols]
+    print(fact.head())
+
+    assert len(fact) == chunk_length
+
+    print(f"Loading Fact table with {len(fact)} rows.")
+
+
+    enable_identity_insert(engine, 'fact_crime_incidents')
+    fact.to_sql("fact_crime_incidents", engine, index=False, if_exists='append')
+    disable_identity_insert(engine, 'fact_crime_incidents')
+    print("Fact table loaded.")
+
+print("All data inserted into SQL Server successfully.")
---
+++ init_db.py
@@ -0,0 +1,93 @@
+from sqlalchemy import (
+    create_engine, MetaData, Table, Column, Integer, BigInteger, String, Float, Boolean, Date, DateTime, ForeignKey, text
+)
+import config
+
+
+# Connection string to 'master' DB (used to create the target DB)
+DB_CONNECTION_STRING_MASTER = (
+    f"mssql+pyodbc://{config.DB_USER}:{config.DB_PASSWORD}@{config.DB_SERVER}/"
+    f"master?driver={config.DB_DRIVER.replace(' ', '+')}"
+)
+
+# -------------------- CREATE DATABASE IF NOT EXISTS --------------------
+engine_master = create_engine(
+    DB_CONNECTION_STRING_MASTER,
+    isolation_level="AUTOCOMMIT",  
+    fast_executemany=True
+)
+
+with engine_master.connect() as conn:
+    conn.execute(text(f"""
+        IF EXISTS (SELECT name FROM sys.databases WHERE name = '{config.DB_NAME}')
+        BEGIN
+            ALTER DATABASE {config.DB_NAME} SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+            DROP DATABASE {config.DB_NAME};
+            CREATE DATABASE {config.DB_NAME};
+        END
+    """))
+    print(f"Database '{config.DB_NAME}' created or already exists.")
+
+# -------------------- CONNECT TO TARGET DATABASE --------------------
+DB_CONNECTION_STRING_TARGET = (
+    f"mssql+pyodbc://{config.DB_USER}:{config.DB_PASSWORD}@{config.DB_SERVER}/"
+    f"{config.DB_NAME}?driver={config.DB_DRIVER.replace(' ', '+')}"
+)
+
+engine = create_engine(DB_CONNECTION_STRING_TARGET, fast_executemany=True)
+metadata = MetaData()
+
+# -------------------- DIMENSION TABLES --------------------
+dim_date = Table('dim_date', metadata,
+    Column('date_id', Integer, autoincrement=True, primary_key=True),  # format: YYYYMMDD
+    Column('full_date', Date),
+    Column('year', Integer),
+    Column('month', Integer),
+    Column('day', Integer),
+    Column('day_of_week', String(10)),
+    Column('is_weekend', Boolean)
+)
+
+dim_location = Table('dim_location', metadata,
+    Column('location_id', Integer, autoincrement=True, primary_key=True),
+    Column('block', String(100)),
+    Column('ward', String(10)),
+    Column('community_area', String(10)),
+    Column('location_description', String(100)),
+    Column('latitude', Float),
+    Column('longitude', Float)
+)
+
+dim_crime_type = Table('dim_crime_type', metadata,
+    Column('crime_type_id', Integer, autoincrement=True, primary_key=True),
+    Column('iucr', String(10)),
+    Column('primary_type', String(50)),
+    Column('description', String(100)),
+    Column('fbi_code', String(10))
+)
+
+# -------------------- FACT TABLE --------------------
+fact_crime_incidents = Table('fact_crime_incidents', metadata,
+    Column('id', BigInteger, primary_key=True),
+    Column('case_number', String(20)),
+    Column('date_id', Integer, ForeignKey('dim_date.date_id')),
+    Column('location_id', Integer, ForeignKey('dim_location.location_id')),
+    Column('crime_type_id', Integer, ForeignKey('dim_crime_type.crime_type_id')),
+    Column('arrest', Boolean),
+    Column('domestic', Boolean),
+    Column('beat', String(10)),
+    Column('district', String(10))
+)
+
+# -------------------- CREATE TABLES --------------------
+metadata.create_all(engine)
+print("✅ Tables created.")
+
+# -------------------- ADD INDEXES --------------------
+with engine.connect() as conn:
+    print("📦 Creating indexes...")
+    conn.execute(text("CREATE INDEX idx_fact_date_id ON fact_crime_incidents(date_id);"))
+    conn.execute(text("CREATE INDEX idx_fact_location_id ON fact_crime_incidents(location_id);"))
+    conn.execute(text("CREATE INDEX idx_fact_crime_type_id ON fact_crime_incidents(crime_type_id);"))
+
+print(f"✅ Schema initialized and indexed in database '{config.DB_NAME}'.")
